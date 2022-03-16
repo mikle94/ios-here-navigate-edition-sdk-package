cb1274874ba6e41cdf9f1d1bf2d5f791039223ce
Mikhail <mikhail@923digital.com>
2022-03-16 19:41:56 +0200

    replaced framework with zip

--- heresdk.xcframework/ios-arm64/heresdk.framework/voice_assets/voice_package_es-AR/content/extract/warning/attributes.lua
+++
@@ -1,8 +0,0 @@
-local attributes = {}
-
-function attributes.is_automatic_avoidance_on(avoidance_mode)
-    return avoidance_mode == 'AUTOMATIC_AVOIDANCE'
-end
-
-
-return attributes
--- heresdk.xcframework/ios-arm64_x86_64-simulator/heresdk.framework/voice_assets/voice_package_hi-IN/translations/hi-IN/mods.lua
+++
@@ -1,3 +0,0 @@
-local mods = require('translations.mods')
-
-return mods
